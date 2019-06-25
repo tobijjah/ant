5859b72d57e3ababe51a2f0a3d362986d7cfd441
tobijjah <tobias.seydewitz@posteo.net>
2019-06-25 19:24:45 +0200

    Update tests and test obstacle

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -8,10 +8,11 @@ Module: test_cell
 from unittest import TestCase
 from unittest.mock import Mock
 
-from ant.agents import Hole
-from ant.agents import KomplexAnt
-from ant.agents import Nutrient
-from ant.agents import Pheromone
+from ant.agents.ant import SimpleAnt
+from ant.agents.hole import Hole
+from ant.agents.nutrient import Nutrient
+from ant.agents.obstacle import Obstacle
+from ant.agents.pheromone import Pheromone
 from ant.environment import Cell
 from ant.environment import Position
 from ant.errors import CellAgentError
@@ -30,8 +31,12 @@ class TestCell(TestCase):
         self.rect.centerx = 1
         self.rect.centery = 1
 
-        self.cell1 = Cell(Position(1, 1), self.rect, 'bar')
-        self.cell2 = Cell(Position(1, 1), self.rect, 'bar')
+        self.surface = Mock()
+        self.surface.get_rect.return_value = self.rect
+
+        self.cell1 = Cell(Position(1, 1), self.surface, self.rect)
+        self.cell2 = Cell(Position(1, 1), self.surface, self.rect)
+        self.cell3 = Cell(Position(1, 1), self.surface, self.rect)
 
     def test_construct(self):
         self.assertFalse(self.cell1.has_hole())
@@ -62,46 +67,69 @@ class TestCell(TestCase):
         self.assertFalse(self.cell1.has_hole())
 
     def test_spawn_nutrient_with_empty_cell(self):
-        actual = self.cell1.spawn_nutrient()
+        actual = self.cell1.spawn_nutrient(1)
 
         self.assertTrue(self.cell1.has_nutrient())
         self.assertTrue(isinstance(actual, Cell))
         self.assertTrue(isinstance(self.cell1.nutrient, Nutrient))
 
     def test_spawn_nutrient_with_occupied_cell(self):
-        self.cell1.spawn_nutrient()
+        self.cell1.spawn_nutrient(1)
 
         with self.assertRaises(CellOccupiedError):
-            self.cell1.spawn_nutrient()
+            self.cell1.spawn_nutrient(1)
 
     def test_get_nutrient_on_empty_cell(self):
         with self.assertRaises(CellAgentError):
             _ = self.cell1.nutrient
 
     def test_delete_nutrient(self):
-        self.cell1.spawn_nutrient()
+        self.cell1.spawn_nutrient(1)
         del self.cell1.nutrient
 
         self.assertFalse(self.cell1.has_nutrient())
 
-    def test_spawn_pheromone_with_empty_cell(self):
-        self.cell1.pheromone = Pheromone(self.rect, 'foo')
+    def test_spawn_obstacle_with_empty_cell(self):
+        actual = self.cell1.spawn_obstacle()
 
-        self.assertTrue(self.cell1.has_pheromone())
-        self.assertTrue(isinstance(self.cell1.pheromone, Pheromone))
+        self.assertTrue(self.cell1.has_obstacle())
+        self.assertTrue(isinstance(actual, Cell))
+        self.assertTrue(isinstance(self.cell1.obstacle, Obstacle))
 
-    def test_spawn_pheromone_with_occupied_cell(self):
-        self.cell1.pheromone = Pheromone(self.rect, 'foo')
+    def test_spawn_obstacle_with_occupied_cell(self):
+        self.cell1.spawn_obstacle()
 
         with self.assertRaises(CellOccupiedError):
-            self.cell1.pheromone = Pheromone(self.rect, 'foo')
+            self.cell1.spawn_obstacle()
+
+    def test_get_obstacle_on_empty_cell(self):
+        with self.assertRaises(CellAgentError):
+            _ = self.cell1.obstacle
+
+    def test_delete_obstacle(self):
+        self.cell1.spawn_obstacle()
+        del self.cell1.obstacle
+
+        self.assertFalse(self.cell1.has_obstacle())
+
+    def test_spawn_pheromone_with_empty_cell(self):
+        pheromone = self.cell1.spawn_pheromone()
+
+        self.assertTrue(self.cell1.has_pheromone())
+        self.assertTrue(isinstance(pheromone, Pheromone))
+
+    def test_spawn_pheromone_with_occupied_cell(self):
+        self.cell1.spawn_pheromone()
+
+        with self.assertRaises(CellOccupiedError):
+            self.cell1.spawn_pheromone()
 
     def test_get_pheromone_on_empty_cell(self):
         with self.assertRaises(CellAgentError):
             _ = self.cell1.pheromone
 
     def test_delete_pheromone(self):
-        self.cell1.pheromone = Pheromone(self.rect, 'foo')
+        self.cell1.spawn_pheromone()
         del self.cell1.pheromone
 
         self.assertFalse(self.cell1.has_pheromone())
@@ -112,9 +140,9 @@ class TestCell(TestCase):
 
     def test_spawn_ant(self):
         self.cell1.spawn_hole()
-        ant = self.cell1.spawn_ant(atype='komplex')
+        ant = self.cell1.spawn_ant()
 
-        self.assertTrue(isinstance(ant, KomplexAnt))
+        self.assertTrue(isinstance(ant, SimpleAnt))
         self.assertTrue(ant.pos == self.cell1.pos)
 
     def test_has_hole(self):
@@ -127,20 +155,29 @@ class TestCell(TestCase):
     def test_has_nutrient(self):
         self.assertFalse(self.cell1.has_nutrient())
 
-        self.cell1.spawn_nutrient()
+        self.cell1.spawn_nutrient(1)
 
         self.assertTrue(self.cell1.has_nutrient())
 
     def test_has_pheromone(self):
         self.assertFalse(self.cell1.has_pheromone())
 
-        self.cell1.pheromone = Pheromone(self.rect, 'foo')
+        self.cell1.spawn_pheromone()
 
         self.assertTrue(self.cell1.has_pheromone())
 
+    def test_has_obstacle(self):
+        self.assertFalse(self.cell1.has_obstacle())
+
+        self.cell1.spawn_obstacle()
+
+        self.assertTrue(self.cell1.has_obstacle())
+
     def test_occupied(self):
         self.cell1.spawn_hole()
-        self.cell2.spawn_nutrient()
+        self.cell2.spawn_nutrient(1)
+        self.cell3.spawn_obstacle()
 
         self.assertTrue(self.cell1.occupied())
         self.assertTrue(self.cell2.occupied())
+        self.assertTrue(self.cell3.occupied())
